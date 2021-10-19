6e0747de1c40e71e9106f3315f8f28fb96360114
jhony-meli <jhony.zuim@mercadolivre.com>
2021-10-19 17:50:45 -0300

    :test_tube: adicionando teste unitario validar nao existencia section

--- src/test/java/br/com/meli/projetointegrador/model/service/SectionServiceTest.java
+++ src/test/java/br/com/meli/projetointegrador/model/service/SectionServiceTest.java
@@ -1,5 +1,6 @@
 package br.com.meli.projetointegrador.model.service;
 
+import br.com.meli.projetointegrador.exception.SectionException;
 import br.com.meli.projetointegrador.model.entity.Section;
 import br.com.meli.projetointegrador.model.entity.Warehouse;
 import br.com.meli.projetointegrador.model.repository.SectionRepository;
@@ -43,4 +44,30 @@ public class SectionServiceTest {
         assertTrue(sectionService.validSection(section));
     }
 
+    @Test
+    void validSectionNotExistTest(){
+
+        Warehouse warehouse = new Warehouse()
+                .warehouseCode("MG")
+                .warehouseName("Sao Paulo")
+                .build();
+
+        Section section = new Section()
+                .id("1")
+                .sectionCode("LA")
+                .sectionName("Laticionios")
+                .maxLength(10)
+                .warehouse(warehouse)
+                .build();
+
+        when(mockSectionRepository.findById(any()))
+                .thenReturn(Optional.empty());
+
+        SectionException sectionException = assertThrows(SectionException.class, () ->
+                sectionService.validSection(section));
+
+        String expectedMessage = "Nao existe esse setor, por gentileza verificar o setor!";
+
+        assertTrue(expectedMessage.contains(sectionException.getMessage()));
+    }
 }
